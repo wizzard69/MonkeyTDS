fbdd600a32f555aa5ae64bc91573d61b9a59fb7f
WizBit <dean@monkiezone.com>
2017-09-28 20:26:04 +0100

    Initial Commit

---
+++ MonkeyTDS/Assets/Scripts/playerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    Camera mainCamera;
+    Rigidbody rb;
+    Vector3 moveInput;
+    Vector3 moveVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+    }
+
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+
+            transform.LookAt(new Vector3(pointToLook.x,transform.position.y,pointToLook.z));
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveVelocity;
+    }
+}
