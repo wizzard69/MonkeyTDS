f1df5f4c95870007d439d629978e155ba385a9cf
WizBit <dean@monkiezone.com>
2017-10-01 18:31:23 +0100

    Damage Feedback

---
+++ MonkeyTDS/Assets/Scripts/HurtPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class HurtPlayer : MonoBehaviour {
+
+    public int dameageToGive;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerHealthManager>().HurtPlayer(dameageToGive);
+        }
+    }
+}
---
+++ MonkeyTDS/Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    public int startHealth;
+    public float flashLength;
+
+    int currentHealth;
+    float flashCounter;
+    Renderer rend;
+    Color baseColor;
+
+    void Start()
+    {
+        currentHealth = startHealth;
+        rend = GetComponent<Renderer>();
+        baseColor = rend.material.GetColor("_Color");
+    }
+
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (flashCounter > 0)
+        {
+            flashCounter -= Time.deltaTime;
+            if (flashCounter <= 0)
+            {
+                rend.material.SetColor("_Color", baseColor);
+            }
+        }
+    }
+
+    public void HurtPlayer(int damage)
+    {
+        currentHealth -= damage;
+        flashCounter = flashLength;
+
+        rend.material.SetColor("_Color", Color.white);
+    }
+}
